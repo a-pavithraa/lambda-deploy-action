0536c0904e50f1a49b9554f6f64cf55246622064
aravamudhan.pavithra@craneww.com <a.pavithraa@gmail.com>
2023-01-30 13:47:48 +0530

    S3 changes

--- main.go
+++ main.go
@@ -1,7 +1,38 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
+)
+
+func NewS3Client(ctx context.Context, region string) (*s3.Client, error) {
+	cfg, err := config.LoadDefaultConfig(ctx, config.WithRegion(region))
+	if err != nil {
+		return nil, fmt.Errorf("unable to load SDK config, %s", err)
+	}
+
+	s3Client := s3.NewFromConfig(cfg, func(o *s3.Options) {
+		o.Region = region
+
+	})
+
+	return s3Client, nil
+}
 
 func main() {
-	fmt.Println("Hello world from go!")
+	s3Client, err := NewS3Client(context.Background(), "us-east-1")
+	if err != nil {
+		panic(err)
+	}
+	response, err := s3Client.ListBuckets(context.Background(), &s3.ListBucketsInput{})
+	if err != nil {
+		panic(err)
+	}
+	for _, bucket := range response.Buckets {
+		fmt.Printf("bucket: %v\n", *bucket.Name)
+	}
+
 }
